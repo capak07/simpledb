75586b8034a4bfaeee90fa6218f6eb758896aec9
Justin <jhabs2002@gmail.com>
2023-12-14 12:16:58 -0400

    add comments

--- src/simpledb/Entites.java
+++ src/simpledb/Entites.java
@@ -10,44 +10,50 @@ import simpledb.tx.Transaction;
 
 public class Entites {
     public static void main(String args[]) {
+        // initialize SimpleDB
         SimpleDB.init("studentdb");
+        
+        // start a transaction
         Transaction tx = new Transaction();
+        
+        // access the metadata manager
         MetadataMgr mdMgr = SimpleDB.mdMgr();
 
-        TableInfo dti = mdMgr.getTableInfo("DEPT", tx);
-        Scan s1 = new TableScan(dti, tx);
-        TableInfo cti = mdMgr.getTableInfo("COURSE", tx);
+        // get TableInfo and create TableScan for student relation
+        TableInfo sti = mdMgr.getTableInfo("student", tx);
+        Scan s1 = new TableScan(sti, tx);
+
+        // get TableInfo and create TableScan for course relation
+        TableInfo cti = mdMgr.getTableInfo("course", tx);
         Scan s2 = new TableScan(cti, tx);
 
-        // Leapfrog triejoin between dept and course on DId=DeptId
-        Scan s3 = new LeapfrogTriejoinScan(s1, s2, new String[] { "DId" }, new String[] { "DeptId" });
+        // perform Leapfrog Triejoin between student and course on MajorId=DeptId
+        Scan s3 = new LeapFrogTrieJoinAlgorithmScan(s1, s2, new String[] { "majorid" }, new String[] { "deptid" });
 
-        // Selection predicate for section
-        Predicate pred3 = new Predicate(new Term(new FieldNameExpression("CId"), new FieldNameExpression("CourseId")));
+        // create a selection predicate for the section
+        Predicate pred3 = new Predicate(new Term(new FieldNameExpression("cid"), new FieldNameExpression("courseid")));
 
-        // Combine the results with the section scan
+        // combine results with the section scan using a ProductScan
         Scan s4 = new ProductScan(s3, s2);
         Scan s5 = new SelectScan(s4, pred3);
 
-        // Selection predicates for the query
-        Predicate pred1 = new Predicate(new Term(new FieldNameExpression("DId"), new FieldNameExpression("DeptId")));
-        Predicate pred2 = new Predicate(new Term(new FieldNameExpression("CId"), new FieldNameExpression("CourseId")));
+        // create selection predicates for the query
+        Predicate pred1 = new Predicate(new Term(new FieldNameExpression("majorid"), new FieldNameExpression("deptid")));
+        Predicate pred2 = new Predicate(new Term(new FieldNameExpression("cid"), new FieldNameExpression("courseid")));
 
-        // Apply selection predicates
+        // apply selection predicates
         Scan s6 = new SelectScan(s5, pred1);
         Scan s7 = new SelectScan(s6, pred2);
 
-        // Project the final result
-        Collection<String> fieldlist = Arrays.asList("DName", "Title", "Prof");
+        // project the final result
+        Collection<String> fieldlist = Arrays.asList("sname", "title", "prof");
         Scan sTop = new ProjectScan(s7, fieldlist);
 
-        // Execute the final scan
-        sTop.beforeFirst(); // Ensure the scan is positioned before the first record
+        // execute the final scan
+        sTop.beforeFirst(); // ensure the scan is positioned before the first record
         while (sTop.next()) {
-            System.out.println(sTop.getString("DName") + "," +
-                    sTop.getString("Title") + "," + sTop.getString("Prof"));
+            System.out.println(sTop.getString("sname") + "," + sTop.getString("title") + "," + sTop.getString("prof"));
         }
-
         sTop.close();
         tx.commit();
     }
--- src/simpledb/query/LeapFrogTrieJoinAlgorithmScan.java
+++ src/simpledb/query/LeapFrogTrieJoinAlgorithmScan.java
@@ -1,137 +1,101 @@
 package simpledb.query;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public class LeapfrogTriejoinScan implements Scan {
-    private Scan outerScan;
-    private Scan innerScan;
-    private String[] outerJoinFields;
-    private String[] innerJoinFields;
-    private List<String> outerValues;
-    private List<String> innerValues;
-    private int outerIndex;
-    private int innerIndex;
+public class LeapFrogTrieJoinAlgorithmScan implements Scan {
 
-    public LeapfrogTriejoinScan(Scan outerScan, Scan innerScan, String[] outerJoinFields, String[] innerJoinFields) {
+    private Scan outerScan, innerScan;
+    // join fields for the outer and inner relations
+    private String[] outerJoinFields, innerJoinFields;
+    private boolean moreOuter = true, moreInner = true;
+
+    public LeapFrogTrieJoinAlgorithmScan(Scan outerScan, Scan innerScan, String[] outerJoinFields, String[] innerJoinFields) {
         this.outerScan = outerScan;
         this.innerScan = innerScan;
         this.outerJoinFields = outerJoinFields;
         this.innerJoinFields = innerJoinFields;
-        this.outerValues = new ArrayList<>();
-        this.innerValues = new ArrayList<>();
-        this.outerIndex = 0;
-        this.innerIndex = -1;
-        loadValues();
+        // initialize scans to the beginning
+        outerScan.beforeFirst();
+        innerScan.beforeFirst();
     }
 
-    private void loadValues() {
-        while (outerScan.next()) {
-            StringBuilder value = new StringBuilder();
-            for (String field : outerJoinFields) {
-                value.append(outerScan.getString(field)).append(",");
-            }
-            outerValues.add(value.toString());
-        }
-
-        while (innerScan.next()) {
-            StringBuilder value = new StringBuilder();
-            for (String field : innerJoinFields) {
-                value.append(innerScan.getString(field)).append(",");
-            }
-            innerValues.add(value.toString());
-        }
-    }
-
+    // reset the scans and flags before the first iteration
     @Override
     public void beforeFirst() {
-        outerIndex = 0;
-        innerIndex = -1;
+        outerScan.beforeFirst();
+        innerScan.beforeFirst();
+        moreOuter = outerScan.next();
+        moreInner = innerScan.next();
     }
 
+    // move to the next valid pair of tuples satisfying the join condition
     @Override
     public boolean next() {
-        while (true) {
-            if (outerIndex == 0 || innerIndex == -1) {
-                // Initial positioning
-                innerIndex = outerIndex < outerValues.size()
-                        ? innerIndex = findMatchingInnerTuple(outerValues.get(outerIndex))
-                        : -1;
-                outerIndex++;
+        while (moreOuter && moreInner) {
+            // retrieve values of the join columns from the outer and inner relations
+            String valOuter = outerScan.getString(outerJoinFields[0]);
+            String valInner = innerScan.getString(innerJoinFields[0]);
+
+            // check if the values are equal, indicating a match
+            if (valOuter.equals(valInner)) {
+                return true;
+            } else if (valOuter.compareTo(valInner) < 0) {
+                // move to the next tuple in the outer relation
+                moreOuter = outerScan.next();
+                if (!moreOuter) {
+                    return false;
+                }
             } else {
-                // Leapfrog Triejoin Algorithm
-                int compareResult = compareValues(outerValues.get(outerIndex), innerValues.get(innerIndex));
-                if (compareResult == 0) {
-                    return true; // Match found
-                } else if (compareResult < 0) {
-                    outerIndex++;
-                } else {
-                    innerIndex = findNextMatchingInnerTuple(outerValues.get(outerIndex), innerIndex + 1);
-                    if (innerIndex == -1) {
-                        outerIndex++;
-                    }
+                // move to the next tuple in the inner relation
+                moreInner = innerScan.next();
+                if (!moreInner) {
+                    return false;
                 }
             }
-
-            // Check if we have reached the end of either scan
-            if (outerIndex >= outerValues.size() || innerIndex >= innerValues.size()
-                    || outerIndex >= innerValues.size()) {
-                return false;
-            }
         }
-    }
-
-    @Override
-    public int getInt(String fldname) {
-        // Implement as needed based on your actual implementation
-        return 0;
-    }
-
-    @Override
-    public String getString(String fldname) {
-        // Implement as needed based on your actual implementation
-        return null;
-    }
-
-    @Override
-    public Constant getVal(String fldname) {
-        // Implement as needed based on your actual implementation
-        return null;
-    }
-
-    @Override
-    public boolean hasField(String fldname) {
-        // Implement as needed based on your actual implementation
         return false;
     }
 
-    private int compareValues(String outerValue, String innerValue) {
-        // Implement value comparison based on join fields
-        // Return 0 if values match, negative if outer < inner, positive if outer >
-        // inner
-        return outerValue.compareTo(innerValue);
-    }
-
-    private int findMatchingInnerTuple(String outerValue) {
-        for (int i = 0; i < innerValues.size(); i++) {
-            if (compareValues(outerValue, innerValues.get(i)) == 0) {
-                return i;
-            }
-        }
-        return -1; // No matching inner tuple found
-    }
-
-    private int findNextMatchingInnerTuple(String outerValue, int startIndex) {
-        for (int i = startIndex; i < innerValues.size(); i++) {
-            if (compareValues(outerValue, innerValues.get(i)) == 0) {
-                return i;
-            }
-        }
-        return -1; // No next matching inner tuple found
-    }
-
+    // close scans
     @Override
     public void close() {
-        // Implement closing logic if necessary
+        outerScan.close();
+        innerScan.close();
+    }
+
+    // get the value of a specified field
+    @Override
+    public Constant getVal(String fldname) {
+        if (Arrays.asList(outerJoinFields).contains(fldname)) {
+            return outerScan.getVal(fldname);
+        } else {
+            return innerScan.getVal(fldname);
+        }
+    }
+
+    // get the integer value of a specified field
+    @Override
+    public int getInt(String fldname) {
+        if (Arrays.asList(outerJoinFields).contains(fldname)) {
+            return outerScan.getInt(fldname);
+        } else {
+            return innerScan.getInt(fldname);
+        }
+    }
+
+    // get the string value of a specified field
+    @Override
+    public String getString(String fldname) {
+        if (Arrays.asList(outerJoinFields).contains(fldname)) {
+            return outerScan.getString(fldname);
+        } else {
+            return innerScan.getString(fldname);
+        }
+    }
+
+    // check if a specified field is present
+    @Override
+    public boolean hasField(String fldname) {
+        return Arrays.asList(outerJoinFields).contains(fldname) || Arrays.asList(innerJoinFields).contains(fldname);
     }
 }
